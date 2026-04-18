cfae25d2cdfa7fd10aeac2ce9d89c554bdfd08ff
Mehmet Emin <c0594313@gmail.com>
2026-04-18 18:12:30 +0300

    implement check-in system for salon owners to verify bookings

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { createBooking } = require("../controllers/bookingController");
-const { protect } = require("../middlewares/authMiddleware");
+const {
+  createBooking,
+  checkInBooking,
+} = require("../controllers/bookingController");
+const { protect, authorize } = require("../middlewares/authMiddleware");
 
 router.post("/", protect, createBooking);
+router.patch("/:id/checkin", protect, authorize("owner"), checkInBooking);
 
 module.exports = router;
